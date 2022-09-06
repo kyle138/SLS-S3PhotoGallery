aa2ffd63915192cbd405cc2c98a2803371386c23
Kyle138 <kyle138@gmail.com>
2022-09-06 00:13:37 -0500

    comments...

--- lambdas/tagger.js
+++ lambdas/tagger.js
@@ -12,17 +12,22 @@ const S3 = new AWS.S3({
 module.exports.handler = async (event) => {
   // console.log('Received event: ' + JSON.stringify(event,null,2)); // DEBUG:
 
+  // *** Check that records, s3, etc etc actually exists...
   let bucket = event.Records[0].s3.bucket.name;
   let key = event.Records[0].s3.object.key;
 
+  // *** if the above exists...
   const resp = await S3.getObjectTagging({
     Bucket: bucket,
     Key: key
   }).promise();
 
+  // *** .then()...
   console.log('resp: ' + JSON.stringify(resp,null,2));  // DEBUG:
 
   let labels = resp?.TagSet.find(o => o.Key === 'labels')?.Value;
   labels = labels?.Value.split(':');
-  console.log('labels: ' + JSON.stringify(labels,null,2));  // DEBUG: 
+  console.log('labels: ' + JSON.stringify(labels,null,2));  // DEBUG:
+
+  // ***.then() add the object name to json for each label...
 };
