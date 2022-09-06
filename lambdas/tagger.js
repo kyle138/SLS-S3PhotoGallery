f5af49c11082d1b2e8d43733bf167279db2c3b0b
Kyle138 <kyle138@gmail.com>
2022-09-05 21:57:29 -0500

    testing GetObjectTaggingCommand()

--- lambdas/tagger.js
+++ lambdas/tagger.js
@@ -1,7 +1,21 @@
 'use strict';
 
+import { S3Client, GetObjectTaggingCommand } from "@aws-sdk/client-s3"
+
+const client = new S3Client({ region: "us-east-1" });
+
 module.exports.handler = async (event) => {
-  console.log('Received event: ' + JSON.stringify(event,null,2)); // DEBUG: 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log('Received event: ' + JSON.stringify(event,null,2)); // DEBUG:
+
+  let bucket = event.Records[0].s3.bucket.name;
+  let key = event.Records[0].s3.object.key;
+
+  const command = new GetObjectTaggingCommand({
+  Bucket: bucket,
+  Key: key
+  });
+
+  const response = await client.send(command);
+  console.log(response);  // DEBUG: 
+
 };
